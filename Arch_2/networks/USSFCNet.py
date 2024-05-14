9d86d919e505d468bcf79283e9706e8a778cb54c
Haitham-AbdelKarim <haithammoabdelkarim@gmail.com>
2024-05-14 17:46:12 +0300

    Unet_Siamese

--- Arch_2/networks/USSFCNet.py
+++ Arch_2/networks/USSFCNet.py
@@ -1,8 +1,30 @@
 import torch.nn as nn
 import torch
+import math
+import numpy as np
 
-from modules.MSDConv_SSFC import MSDConv_SSFC
+# from modules.MSDConv_SSFC import MSDConv_SSFC
+# from modules.SSFC import SSFC
 
+class SSFC(torch.nn.Module):
+    def __init__(self, in_ch):
+        super(SSFC, self).__init__()
+
+        # self.proj = nn.Conv2d(in_ch, in_ch, kernel_size=1)  # generate k by conv
+
+    def forward(self, x):
+        _, _, h, w = x.size()
+
+        q = x.mean(dim=[2, 3], keepdim=True)
+        # k = self.proj(x)
+        k = x
+        square = (k - q).pow(2)
+        sigma = square.sum(dim=[2, 3], keepdim=True) / (h * w)
+        att_score = square / (2 * sigma + np.finfo(np.float32).eps) + 0.5
+        att_weight = nn.Sigmoid()(att_score)
+        # print(sigma)
+
+        return x * att_weight
 
 class First_DoubleConv(nn.Module):
     def __init__(self, in_ch, out_ch):
@@ -18,15 +40,48 @@ class First_DoubleConv(nn.Module):
 
     def forward(self, input):
         return self.conv(input)
+    
+class MSDConv_SSFC(nn.Module):
+    def __init__(self, in_ch, out_ch, kernel_size=1, stride=1, padding=0, ratio=2, aux_k=3, dilation=3):
+        super(MSDConv_SSFC, self).__init__()
+        self.out_ch = out_ch
+        native_ch = math.ceil(out_ch / ratio)
+        aux_ch = native_ch * (ratio - 1)
+
+        # native feature maps
+        self.native = nn.Sequential(
+            nn.Conv2d(in_ch, native_ch, kernel_size, stride, padding=padding, dilation=1, bias=False),
+            nn.BatchNorm2d(native_ch),
+            nn.ReLU(inplace=True),
+        )
+
+        # auxiliary feature maps
+        self.aux = nn.Sequential(
+            nn.Conv2d(native_ch, aux_ch, aux_k, 1, padding=1, groups=int(native_ch / 4), dilation=dilation,
+                   bias=False),
+            nn.BatchNorm2d(aux_ch),
+            nn.ReLU(inplace=True),
+        )
+
+        self.att = SSFC(aux_ch)
+
+    def forward(self, x):
+        x1 = self.native(x)
+        x2 = self.att(self.aux(x1))
+        out = torch.cat([x1, x2], dim=1)
+        return out[:, :self.out_ch, :, :]
 
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self, in_ch, out_ch, kernel_size=1, stride=1, padding=0, ratio=2, aux_k=3, dilation=3):
         super(DoubleConv, self).__init__()
+        # native_ch = math.ceil(out_ch / ratio)
+        # aux_ch = native_ch * (ratio - 1)
         self.Conv = nn.Sequential(
             MSDConv_SSFC(in_ch, out_ch, dilation=3),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
+            
             MSDConv_SSFC(out_ch, out_ch, dilation=3),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True)
